6984f633b5ca83fa5c34a69776e6645a8b216e3e
vvmokrenko <v.v.mokrenko@gmail.com>
2021-07-18 18:45:22 +0300

    Lesson 2 HomeWork 1. Добавлена статика в index и products 2. В index добавлен вывод текущей даты через шаблонный фильтр 3. Сделана динамика для отображения заголовков страниц и перечня адресов контактов. 4. Список продуктов забирается из JSON-файла.

--- geekshop/views.py
+++ geekshop/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'geekshop/index.html')
+    title = 'дОМОй'
+
+    context = {
+        'title': title,
+        'slogan': 'супер предложения',
+    }
+
+    return render(request, 'geekshop/index.html', context)
 
 
 def contacts(request):
-    return render(request, 'geekshop/contact.html')
+    title = 'КОНтакты'
+
+    address_list = [
+        {'city': 'Москва', 'phone': '+7-495-888-8888', 'email': 'info@geekshop.ru', 'address': 'В пределах МКАД'},
+        {'city': 'Санкт-Петербург', 'phone': '+7-812-888-8888', 'email': 'info@geekshop.spb.ru', 'address': 'В пределах КАД'},
+        {'city': 'Краснодар', 'phone': '+7-861-888-8888', 'email': 'info@geekshop.krd.ru', 'address': 'Уточнить по телефону'},
+    ]
+
+    context = {
+        'title': title,
+        'address_list': address_list,
+    }
+
+    return render(request, 'geekshop/contact.html', context)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+import json
+
+def get_links_menu_from_file(filename):
+    with open(filename, 'r', encoding='utf-8') as f:  # открыли файл с данными
+        text = json.load(f)  # загнали все, что получилось в переменную
+    return text['links_menu'] # возвратили массив ссылок
 
 
-def products(request):
-    return render(request, 'mainapp/products.html')
 
+def index(request):
+    title = 'кателог'
+
+    links_menu = [
+        {'href': 'index', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    context2 =  {
+        'title': title,
+        'links_menu': get_links_menu_from_file('mainapp/products.json'),
+    }
+    return render(request, 'mainapp/products.html', context2)
